ecdc916d1f91babb81985a575559eeb3b1da3ce7
Cedar8344 <jerryang1023@gmail.com>
2020-07-03 12:20:35 -0700

    aaa

--- CANtest.go
+++ CANtest.go
@@ -19,7 +19,7 @@ func main() {
 		id := uint32(ids)
 		//b, errr := strconv.ParseInt(records[i][4], 16, 64)
 		c, errr := strconv.ParseInt(records[i][5]+records[i][4], 16, 64)
-		fc = float64(c)
+		fc := float64(c)
 		current := (fc)/1000.0
 		//d, errr := strconv.ParseInt(records[i][6], 16, 64)
 		e, errr := strconv.ParseInt(records[i][7]+records[i][6], 16, 64)
